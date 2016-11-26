b758ae476a570713a683510fb4b206b9fb63b440
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-11-26 16:37:09 +0300

    Implement sign up page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './js/App';
+import SignUpPage from './js/SignUpPage'
+
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#2D3C55',
+  },
+});
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={() => <div>Hello</div>} />
-    </Route>
-  </Router>,
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={() => <div>Hello</div>} />
+        <Route path="signup" component={SignUpPage} />
+      </Route>
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
---
+++ src/js/SignUpPage/Component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './styles.css'
+import Logo from '../Logo'
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+
+export default () => (
+  <div className="sign-up-page">
+    <div className="sign-up-page__top-background" />
+    <div className="sign-up-page__card">
+      <div className="sign-up-page__card-header">
+        <Logo />
+      </div>
+      <div className="sign-up-page__card-body">
+        <h1 className="sign-up-page__card-title">Sign up</h1>
+        <p className="sign-up-page__card-description">
+          Please create account or login into existing one.
+        </p>
+        <div className="sign-up-page__inputs">
+          <TextField className="sign-up-page__input" floatingLabelText="Email" />
+          <TextField className="sign-up-page__input" floatingLabelText="Password" />
+        </div>
+      </div>
+      <div className="sign-up-page__buttons">
+        <FlatButton label="Next" />
+      </div>
+    </div>
+  </div>
+)
