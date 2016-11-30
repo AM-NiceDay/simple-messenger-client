56f7bc871365b5eca6a0d41423ac69b3cf227a58
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-11-30 13:12:40 +0300

    Update messenger body

--- src/js/Messenger/Component.jsx
+++ src/js/Messenger/Component.jsx
@@ -61,33 +61,49 @@ export default () => (
         </div>
       </div>
       <div className="messenger__chat-messages">
-        <div className="messenger__chat-message messenger__chat-message--out">
-          <div className="messenger-chat-message-body">
-            <div className="messenger__chat-message-content messenger__chat-message-content--out">Sure, it's okey</div>
-            <span className="messenger__chat-message-time">9:09 PM</span>
+        <div className="messenger__chat-message">
+          <img className="messenger__chat-message-photo" src="https://randomuser.me/api/portraits/men/50.jpg" alt="user-image" />
+          <div className="messenger__chat-message-content">
+            <div className="messenger__chat-message-content-header">
+              <span className="messenger__chat-message-name">Douglas Mckinney</span>
+              <span className="messenger__chat-message-time">9:08 PM</span>
+            </div>
+            <span className="messenger__char-message-content-body">Sure, it's okey</span>
           </div>
         </div>
-        <div className="messenger__chat-message messenger__chat-message--out">
-          <div className="messenger-chat-message-body">
-            <div className="messenger__chat-message-content messenger__chat-message-content--out">Fine. I was thinking about your suggest. What do you think if I will take a book for a few weeks?</div>
-            <span className="messenger__chat-message-time">9:08 PM</span>
+        <div className="messenger__chat-message">
+          <img className="messenger__chat-message-photo" src="https://randomuser.me/api/portraits/men/85.jpg" alt="user-image" />
+          <div className="messenger__chat-message-content">
+            <div className="messenger__chat-message-content-header">
+              <span className="messenger__chat-message-name">Mae Lucas</span>
+              <span className="messenger__chat-message-time">9:08 PM</span>
+            </div>
+            <span className="messenger__char-message-content-body">Fine. I was thinking about your suggest. What do you think if I will take a book for a few weeks?</span>
           </div>
         </div>
-        <div className="messenger__chat-message messenger__chat-message--in">
-          <div className="messenger-chat-message-body">
-            <div className="messenger__chat-message-content messenger__chat-message-content--in">Hey, going well. U?</div>
-            <span className="messenger__chat-message-time">8:52 PM</span>
+        <div className="messenger__chat-message">
+          <img className="messenger__chat-message-photo" src="https://randomuser.me/api/portraits/men/50.jpg" alt="user-image" />
+          <div className="messenger__chat-message-content">
+            <div className="messenger__chat-message-content-header">
+              <span className="messenger__chat-message-name">Douglas Mckinney</span>
+              <span className="messenger__chat-message-time">8:52 PM</span>
+            </div>
+            <span className="messenger__char-message-content-body">Hey, going well. U?</span>
           </div>
         </div>
-        <div className="messenger__chat-message messenger__chat-message--out">
-          <div className="messenger-chat-message-body">
-            <div className="messenger__chat-message-content messenger__chat-message-content--out">How is it going?</div>
-            <span className="messenger__chat-message-time">8:48 PM</span>
+        <div className="messenger__chat-message">
+          <div className="messenger__chat-message-content">
+            <span className="messenger__char-message-content-body">How is it going?</span>
           </div>
         </div>
-        <div className="messenger__chat-message messenger__chat-message--out">
-          <div className="messenger-chat-message-body">
-            <div className="messenger__chat-message-content messenger__chat-message-content--out">Hello</div>
+        <div className="messenger__chat-message">
+          <img className="messenger__chat-message-photo" src="https://randomuser.me/api/portraits/men/85.jpg" alt="user-image" />
+          <div className="messenger__chat-message-content">
+            <div className="messenger__chat-message-content-header">
+              <span className="messenger__chat-message-name">Mae Lucas</span>
+              <span className="messenger__chat-message-time">8:47 PM</span>
+            </div>
+            <span className="messenger__char-message-content-body">Hello</span>
           </div>
         </div>
       </div>
