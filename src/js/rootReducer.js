823347db3bd3feff3ba8a46609337245449724b8
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-12-04 14:25:59 +0300

    Implement data module

--- src/js/rootReducer.js
+++ src/js/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { dataReducer } from './modules/data';
 import auth from './features/auth/duck';
 import messenger from './features/Messenger/duck';
 
 export default combineReducers({
   auth,
   messenger,
+  data: dataReducer,
   routing: routerReducer,
 });
