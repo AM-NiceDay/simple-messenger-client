bca223aebc5387fe2d5bb6c3baf846145ec4416a
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-12-08 19:20:12 +0300

    Update last message in chat

--- src/js/features/Messenger/sagas.js
+++ src/js/features/Messenger/sagas.js
@@ -1,7 +1,7 @@
 import { takeEvery } from 'redux-saga';
 import { put } from 'redux-saga/effects';
 import api from '../../modules/api';
-import { pushItems } from '../../modules/data';
+import { pushItem, pushItems } from '../../modules/data';
 import { FETCH_CHATS, fetchChatsSuccess } from './actions';
 
 function* fetchChatsSaga () {
@@ -9,6 +9,10 @@ function* fetchChatsSaga () {
 
   yield put(pushItems('users', users));
   yield put(pushItems('messages', messages));
+  yield chats.map(chat => put(pushItem('chatMetas', {
+    _id: chat._id,
+    messageIds: [chat.lastMessageId],
+  })));
   yield put(pushItems('chats', chats));
   yield put(fetchChatsSuccess(chats));
 }
--- src/js/features/Messenger/selectors.js
+++ src/js/features/Messenger/selectors.js
@@ -1,11 +1,16 @@
 import { getItem, getItems } from '../../modules/data';
 
+const getLastMessageId = (state, chatId) => {
+  const chatMessageIds = getItem(state, 'chatMetas', chatId).messageIds;
+  return chatMessageIds[chatMessageIds.length - 1];
+};
+
 export const getChats = state => {
   const chats = getItems(state, 'chats', state.messenger.chatIds);
   const populatedChats = chats.map(chat => ({
     ...chat,
     users: getItems(state, 'users', chat.userIds),
-    lastMessage: getItem(state, 'messages', chat.lastMessageId),
+    lastMessage: getItem(state, 'messages', getLastMessageId(state, chat._id)),
   }));
 
   return populatedChats;
