4a4b72da3f2fe37ef7fd14505ade5eb8fb1dd31d
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-12-21 19:03:54 +0300

    Partialy created chat-list selector

--- src/js/features/ChatList/selectors.js
+++ src/js/features/ChatList/selectors.js
@@ -1,10 +1,21 @@
+import { createSelector } from 'reselect';
 import { getChats, getMessages, getUser } from '../core/data';
 
-export const getChatListChats = state => {
-  return getChats(state, state.chatList.chatIds)
-    .map(chat => ({
-      ...chat,
-      peer: getUser(state, chat.peerId),
-      // lastMessage: getMessages(state, getLastMessageId(state, chat._id)),
-    }));
-}
+const getChatListChatIds = state => state.chatList.chatIds; // ['c1', 'c2', 'c3']
+const getChatListChats = createSelector(
+  getChatListChatIds,
+  chatIds => chatIds.map(chatId => getChats())
+);
+
+export const getChatListChats = createSelector(
+  [getChats, getMessages, getUser]
+);
+
+// export const getChatListChats = state => {
+//   return getChats(state, state.chatList.chatIds)
+//     .map(chat => ({
+//       ...chat,
+//       peer: getUser(state, chat.peerId),
+//       // lastMessage: getMessages(state, getLastMessageId(state, chat._id)),
+//     }));
+// };
