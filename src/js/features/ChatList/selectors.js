69197c858b8beb459bcb1bca5adfb438b95c4693
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-12-22 13:18:11 +0300

    ChatList implement getChatListChats selector

--- src/js/features/ChatList/selectors.js
+++ src/js/features/ChatList/selectors.js
@@ -1,21 +1,11 @@
-import { createSelector } from 'reselect';
-import { getChats, getMessages, getUser } from '../core/data';
+import sortBy from 'lodash/fp/sortBy';
+import { getChats, getChatLatestMessage, getMessages, getUser } from '../core/data';
 
-const getChatListChatIds = state => state.chatList.chatIds; // ['c1', 'c2', 'c3']
-const getChatListChats = createSelector(
-  getChatListChatIds,
-  chatIds => chatIds.map(chatId => getChats())
-);
-
-export const getChatListChats = createSelector(
-  [getChats, getMessages, getUser]
-);
-
-// export const getChatListChats = state => {
-//   return getChats(state, state.chatList.chatIds)
-//     .map(chat => ({
-//       ...chat,
-//       peer: getUser(state, chat.peerId),
-//       // lastMessage: getMessages(state, getLastMessageId(state, chat._id)),
-//     }));
-// };
+export const getChatListChats = state => {
+  return getChats(state, state.chatList.chatIds)
+    .map(chat => ({
+      ...chat,
+      peer: getUser(state, chat.peerId),
+      lastMessage: getChatLatestMessage(state, chat._id),
+    }));
+};
---
+++ src/js/features/ChatList/selectors.spec.js
@@ -0,0 +1,45 @@
+import { getChatListChats } from './selectors';
+
+describe('ChatList selectors', () => {
+  describe('#getChatListChats', () => {
+    it('returns populated chats', () => {
+      const state = {
+        chatList: {
+          chatIds: ['c1', 'c2', 'c3'],
+        },
+        data: {
+          chats: {
+            c1: { _id: 'c1', peerId: 'u1' },
+            c2: { _id: 'c2', peerId: 'u2' },
+            c3: { _id: 'c3', peerId: 'u3' },
+          },
+          chatsMessages: {
+            c1: ['m1', 'm2'],
+            c2: ['m3', 'm4'],
+          },
+          messages: {
+            m1: { _id: 'm1', created: '2016-12-11T18:18:50.172Z' },
+            m2: { _id: 'm2', created: '2016-12-11T18:18:53.172Z' },
+            m3: { _id: 'm3', created: '2016-12-11T18:18:56.172Z' },
+            m4: { _id: 'm4', created: '2016-12-11T18:18:51.172Z' },
+          },
+          users: {
+            u1: { _id: 'u1' },
+            u2: { _id: 'u2' },
+            u3: { _id: 'u3' },
+          },
+        },
+      };
+
+      const chats = getChatListChats(state);
+
+      expect(chats).toEqual(
+        [
+          { _id: 'c1', peerId: 'u1', peer: { _id: 'u1' }, lastMessage: { _id: 'm2', created: '2016-12-11T18:18:53.172Z' } },
+          { _id: 'c2', peerId: 'u2', peer: { _id: 'u2' }, lastMessage: { _id: 'm3', created: '2016-12-11T18:18:56.172Z' } },
+          { _id: 'c3', peerId: 'u3', peer: { _id: 'u3' } },
+        ]
+      );
+    });
+  });
+});
