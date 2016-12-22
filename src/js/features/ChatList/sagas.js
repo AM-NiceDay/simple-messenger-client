2eba4b80403db9dd23a50aded9c69d54191d764b
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-12-22 13:17:48 +0300

    ChatList fix loading [null] chatMessages

--- src/js/features/ChatList/sagas.js
+++ src/js/features/ChatList/sagas.js
@@ -4,14 +4,21 @@ import api from '../core/api';
 import { loadChats, loadChatsMessages, loadMessages, loadUsers } from '../core/data';
 import { FETCH_CHATS, fetchChatsLoading, fetchChatsSuccess } from './actions';
 
+export function* loadData({ chats, messages, users }) {
+  yield put(loadChats(chats));
+  yield put(loadChatsMessages(chats.map(chat => ({
+    chatId: chat._id,
+    messageIds: chat.lastMessageId ? [chat.lastMessageId] : undefined,
+  }))));
+  yield put(loadMessages(messages));
+  yield put(loadUsers(users));
+}
+
 export function* fetchChatsSaga() {
   yield put(fetchChatsLoading());
 
   const { chats, messages, users } = yield call(api.chats.getChats);
-  yield put(loadChats(chats));
-  yield put(loadChatsMessages(chats.map(chat => ({ chatId: chat._id, messageIds: [chat.lastMessageId] }))));
-  yield put(loadMessages(messages));
-  yield put(loadUsers(users));
+  yield call(loadData, { chats, messages, users });
   yield put(fetchChatsSuccess(chats));
 }
 
--- src/js/features/ChatList/sagas.spec.js
+++ src/js/features/ChatList/sagas.spec.js
@@ -1,14 +1,13 @@
 import { put, call } from 'redux-saga/effects';
 import api from '../core/api';
-import { fetchChatsSaga } from './sagas';
+import { fetchChatsSaga, loadData } from './sagas';
 import { FETCH_CHATS, fetchChatsLoading, fetchChatsSuccess } from './actions';
-import { loadChats, loadMessages, loadChatsMessages, loadUsers } from '../core/data';
+import { loadChats, loadChatsMessages, loadMessages, loadUsers } from '../core/data';
 
 describe('ChatList sagas', () => {
-  describe('#fetchChatsSaga', () => {
-    it('works correctly', () => {
-      const gen = fetchChatsSaga();
-      const apiResponse = {
+  describe('#loadData', () => {
+    it('loads data', () => {
+      const data = {
         chats: [
           { _id: 'c1', userIds: ['u1', 'u2'], lastMessageId: 'm1' },
           { _id: 'c2', userIds: ['u1', 'u3'], lastMessageId: 'm2' },
@@ -23,14 +22,9 @@ describe('ChatList sagas', () => {
           { _id: 'm2' },
         ],
       };
+      const gen = loadData(data);
 
       expect(gen.next().value).toEqual(
-        put(fetchChatsLoading())
-      );
-      expect(gen.next().value).toEqual(
-        call(api.chats.getChats)
-      );
-      expect(gen.next(apiResponse).value).toEqual(
         put(loadChats([
           { _id: 'c1', userIds: ['u1', 'u2'], lastMessageId: 'm1' },
           { _id: 'c2', userIds: ['u1', 'u3'], lastMessageId: 'm2' },
@@ -54,6 +48,57 @@ describe('ChatList sagas', () => {
           { _id: 'u2' },
           { _id: 'u3' },
         ]))
+      );
+    });
+    it('loads chatMessages without messageIds if chat does not have lastMessageId', () => {
+      const data = {
+        chats: [{ _id: 'c1' }],
+        messages: [],
+        users: [],
+      };
+      const gen = loadData(data);
+
+      expect(gen.next().value).toEqual(
+        put(loadChats([{ _id: 'c1' }]))
+      );
+      expect(gen.next().value).toEqual(
+        put(loadChatsMessages([{ chatId: 'c1' }]))
+      );
+      expect(gen.next().value).toEqual(
+        put(loadMessages([]))
+      );
+      expect(gen.next().value).toEqual(
+        put(loadUsers([]))
+      )
+    });
+  });
+
+  describe('#fetchChatsSaga', () => {
+    it('works correctly', () => {
+      const gen = fetchChatsSaga();
+      const apiResponse = {
+        chats: [
+          { _id: 'c1', userIds: ['u1', 'u2'], lastMessageId: 'm1' },
+          { _id: 'c2', userIds: ['u1', 'u3'], lastMessageId: 'm2' },
+        ],
+        users: [
+          { _id: 'u1' },
+          { _id: 'u2' },
+          { _id: 'u3' },
+        ],
+        messages: [
+          { _id: 'm1' },
+          { _id: 'm2' },
+        ],
+      };
+      expect(gen.next().value).toEqual(
+        put(fetchChatsLoading())
+      );
+      expect(gen.next().value).toEqual(
+        call(api.chats.getChats)
+      );
+      expect(gen.next(apiResponse).value).toEqual(
+        call(loadData, apiResponse)
       );
       expect(gen.next().value).toEqual(
         put(fetchChatsSuccess([
